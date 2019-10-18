eec7b8e9b578ebb299cd9584b475a4db5efbc399
Tomas Bahnik <tomas.bahnik@email.cz>
2019-10-18 09:55:25 +0200

    formatted version of `sample.py`

--- sedgewick/sample.py
+++ sedgewick/sample.py
@@ -1,6 +1,6 @@
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # sample.py
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 import stdio
 import sys
@@ -11,8 +11,8 @@ import random
 # output a random sample of m integers in the range 0...n-1 (no
 # duplicates).
 
-m = int(sys.argv[1]) # choose this many elements
-n = int(sys.argv[2]) # from 0, 1, ..., n-1
+m = int(sys.argv[1])  # choose this many elements
+n = int(sys.argv[2])  # from 0, 1, ..., n-1
 
 # Initialize perm.
 perm = stdarray.create1D(n, 0)
@@ -21,7 +21,6 @@ for i in range(n):
 
 # Create random sample in perm[0], perm[1], ..., perm[m-1]
 for i in range(m):
-
     # Choose a random integer r between i and n-1.
     r = random.randrange(i, n)
 
@@ -35,7 +34,7 @@ for i in range(m):
     stdio.write(str(perm[i]) + ' ')
 stdio.writeln()
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 # python sample.py 6 16      
 # 9 6 3 5 4 13
@@ -44,5 +43,4 @@ stdio.writeln()
 # 566 240 662 680 500 491 309 348 563 34 
 
 # python sample.py 20 20  
-# 15 6 19 5 1 4 10 16 7 3 13 14 12 2 9 0 18 8 17 11 
-
+# 15 6 19 5 1 4 10 16 7 3 13 14 12 2 9 0 18 8 17 11
