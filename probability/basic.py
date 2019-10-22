0feb2bf86661402dc5ba3b187ba251fd7efaca3a
Tomas Bahnik <tomas.bahnik@email.cz>
2019-10-22 13:12:55 +0200

    sampling added

--- probability/basic.py
+++ probability/basic.py
@@ -1,9 +1,13 @@
 from mxnet import nd
 from matplotlib import pyplot as plt
+import random
+import numpy as np
+import math
 
 # number of outcomes - sample space
 # events are subsets of the sample space, elementary events are individual outcomes
 # random variable is, in this case, identity function X(zero) = 0.., X(two) = 2 ...
+STOP = 101  # 100001
 n = 6  # outcomes are 0...n-1
 probabilities = nd.ones(n) / n
 number_of_tosses = 10000
@@ -41,6 +45,21 @@ def print_estimated_pr(estimates):
     probabilities_after_tosses(number_of_tosses, estimates)
 
 
+def uniform_sampling():
+    counts = np.zeros(100)
+    fig, axes = plt.subplots(2, 2, sharex=True)
+    axes = axes.flatten()
+    # Mangle subplots such that we can index them in a linear fashion rather than
+    # a 2D grid
+    for i in range(1, STOP):
+        randint = random.randint(0, 99)
+        counts[randint] += 1
+        if i in [10, 101]:
+            axes[int(math.log10(i)) - 2].bar(np.arange(1, 101), counts)
+            plt.show()
+        # plt.savefig('uniform_sampling.' + figure_format, dpi=300)
+
+
 def plot_figure(estimated_pr):
     set_figure_size((12, 8))
     for i in range(6):
@@ -71,8 +90,7 @@ def arrays():
 
 
 def main():
-    rolling()
-    # arrays()
+    uniform_sampling()
 
 
 if __name__ == '__main__':
