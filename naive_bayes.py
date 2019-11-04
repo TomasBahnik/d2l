ddfa111ee1583fea5888cdd4ca438a49dacadfbc
Tomas Bahnik <tomas.bahnik@email.cz>
2019-11-04 10:34:56 +0100

    verify prediction for one image

--- naive_bayes.py
+++ naive_bayes.py
@@ -18,21 +18,14 @@ image_size = 28
 train_images, train_labels = mnist_train[:]  # all training examples
 
 
-def main():
-    train_model(train_images, train_labels)
-
-
-def train_model(images, labels):
-    l_c = label_counts(labels)
-    label_probabilities = l_c / l_c.sum()
-    for label, probability in enumerate(label_probabilities):
-        print('label {} : {}'.format(label, probability))
-    print('Samples', nd.size_array(images))
-    P_xy = label_pixel_probabilities(images, labels, l_c)
-    # test the prediction
-    image, label = mnist_test[0]
-    prediction = bayes_pred(image, label_probabilities, P_xy)
-    print(prediction)
+def show_samples():
+    image, label = mnist_train[2]
+    print(image.shape, label, type(image))
+    print(label, type(label), label.dtype)
+    images, labels = mnist_train[0:4:1]
+    print(images.shape, labels.shape)
+    d2l.show_images(images, 2, 9)
+    plt.show()
 
 
 def label_counts(labels):
@@ -62,26 +55,63 @@ def label_pixel_probabilities(images, labels, lbl_counts):
     # Laplace smoothing, add 1
     label_counts_smoothed_reshaped = (lbl_counts + 1).reshape((10, 1, 1))
     P_xy = (label_pixel_counts + 1) / label_counts_smoothed_reshaped
-    d2l.show_images(P_xy, 5, 2)  # 2x5=10
-    plt.show()
+    # show pixel probabilities
+    # d2l.show_images(P_xy, 5, 2)  # 2x5=10
+    # plt.show()
     return P_xy
 
 
-def bayes_pred(x, P_y, P_xy):
+# multiplication of small probabilities leads to zeroes
+def bayes_pred_underflow(x, P_y, P_xy):
     x = x.expand_dims(axis=0)  # (28, 28) -> (1, 28, 28)
     p_xy = P_xy * x + (1 - P_xy) * (1 - x)
     p_xy = p_xy.reshape((10, -1)).prod(axis=1)  # p(x|y)
     return p_xy * P_y
 
 
-def show_samples():
-    image, label = mnist_train[2]
-    print(image.shape, label, type(image))
-    print(label, type(label), label.dtype)
-    images, labels = mnist_train[0:4:1]
-    print(images.shape, labels.shape)
-    d2l.show_images(images, 2, 9)
-    plt.show()
+# logarithm of probabilities product
+
+
+def bayes_pred_stable(x, P_y, P_xy):
+    log_P_xy = nd.log(P_xy)
+    log_P_xy_neg = nd.log(1 - P_xy)
+    log_P_y = nd.log(P_y)
+    x = x.expand_dims(axis=0)  # (28, 28) -> (1, 28, 28)
+    p_xy = log_P_xy * x + log_P_xy_neg * (1 - x)
+    p_xy = p_xy.reshape((10, -1)).sum(axis=1)  # p(x|y)
+    return p_xy + log_P_y
+
+
+def train_model(images, labels):
+    l_c = label_counts(labels)
+    # label probabilities
+    P_y = l_c / l_c.sum()
+    for label, probability in enumerate(P_y):
+        print('label {} : {}'.format(label, probability))
+    print('Samples', nd.size_array(images))
+    P_xy = label_pixel_probabilities(images, labels, l_c)
+    # test the prediction
+    image, label = mnist_test[1]
+    predict_one(image, label, P_y, P_xy)
+
+
+# verify prediction for one image
+def predict_one(x, y, P_y, P_xy):
+    # label probability predictions
+    py = bayes_pred_stable(x, P_y, P_xy)
+    print(py)
+    # Check if the label prediction is correct
+    predicted_label = int(py.argmax(axis=0).asscalar())
+    print('predicted label {} : actual label {} : match {}'.format(predicted_label, y, predicted_label == y))
+
+
+# prediction for set of images
+def predict(X, P_y, P_xy):
+    return [bayes_pred_stable(x, P_y, P_xy).argmax(axis=0).asscalar() for x in X]
+
+
+def main():
+    train_model(train_images, train_labels)
 
 
 if __name__ == '__main__':
