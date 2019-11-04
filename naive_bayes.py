ed3123239757064864bd28a6a29954620c7024e9
Tomas Bahnik <tomas.bahnik@email.cz>
2019-11-04 09:54:53 +0100

    add comments and first prediction

--- naive_bayes.py
+++ naive_bayes.py
@@ -13,13 +13,13 @@ def transform(data, label):
 mnist_train = gluon.data.vision.MNIST(train=True, transform=transform)
 mnist_test = gluon.data.vision.MNIST(train=False, transform=transform)
 
-number_of_classes = 10
+number_of_classes = 10  # 0 .. 9
 image_size = 28
-mnist_images, mnist_labels = mnist_train[:]  # all training examples
+train_images, train_labels = mnist_train[:]  # all training examples
 
 
 def main():
-    train_model(mnist_images, mnist_labels)
+    train_model(train_images, train_labels)
 
 
 def train_model(images, labels):
@@ -28,10 +28,23 @@ def train_model(images, labels):
     for label, probability in enumerate(label_probabilities):
         print('label {} : {}'.format(label, probability))
     print('Samples', nd.size_array(images))
-    label_pixel_probabilities(images, labels, l_c)
+    P_xy = label_pixel_probabilities(images, labels, l_c)
+    # test the prediction
+    image, label = mnist_test[0]
+    prediction = bayes_pred(image, label_probabilities, P_xy)
+    print(prediction)
 
 
 def label_counts(labels):
+    """Returns a new array where element at index = `label` contains the count of the `label`
+     in `labels`
+
+        Parameters
+        ----------
+        labels : int type labels
+            The 1D arrays of int valued labels in [0,number_of_classes)
+        """
+
     ret_val = nd.zeros(number_of_classes)
     for label in range(number_of_classes):
         ret_val[label] = (labels == label).sum()
@@ -46,6 +59,7 @@ def label_pixel_probabilities(images, labels, lbl_counts):
         # numpy arrays : element wise sum = total counts of pixels for given label because pixel is 0 or 1
         label_pixel_counts_numpy = label_images.sum(axis=0)
         label_pixel_counts[label] = nd.array(label_pixel_counts_numpy)
+    # Laplace smoothing, add 1
     label_counts_smoothed_reshaped = (lbl_counts + 1).reshape((10, 1, 1))
     P_xy = (label_pixel_counts + 1) / label_counts_smoothed_reshaped
     d2l.show_images(P_xy, 5, 2)  # 2x5=10
