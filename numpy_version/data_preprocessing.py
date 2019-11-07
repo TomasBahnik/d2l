cd029b328e0a7bf4afbe27f63944e28b552ed4e7
Tomas Bahnik <tomas.bahnik@email.cz>
2019-11-07 18:24:14 +0100

    data processing ndarray

--- numpy_version/data_preprocessing.py
+++ numpy_version/data_preprocessing.py
@@ -1,13 +1,18 @@
 from mxnet import np, npx
 import sys
 import pandas as pd
+from mxnet import np
 
 data_file = '../data/house_tiny.csv'
 
 
 def read_csv():
     data = pd.read_csv(data_file)
-    print(data)
+    inputs, outputs = data.iloc[:, 0:2], data.iloc[:, 2]
+    inputs = inputs.fillna(inputs.mean())
+    inputs = pd.get_dummies(inputs, dummy_na=True)
+    X, y = np.array(inputs.values), np.array(outputs.values)
+    print("examples '{}', labels '{}'".format(X, y))
 
 
 def write_csv():
