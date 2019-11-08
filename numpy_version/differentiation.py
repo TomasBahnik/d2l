8eb4aac2290beacf4fa4bfc49ba1ce121f3311ad
Tomas Bahnik <tomas.bahnik@email.cz>
2019-11-08 16:49:24 +0100

    compute gradient for externally defined function

--- numpy_version/differentiation.py
+++ numpy_version/differentiation.py
@@ -3,25 +3,28 @@ from mxnet import autograd, np, npx
 
 npx.set_np()
 
-# the point where the gradient
-# for the scalar function y = 2 * np.dot(x, x) i.e. 2 * l_2(x)^2
-# is calculated. The gradient is (vector) 4x
-x = np.arange(4)
-x.attach_grad()
+
+def f(a):
+    return np.dot(a, a)
 
 
-def gradient():
+# x : the point where the gradient
+# for the scalar function f(x) is calculated
+def grad_f_at(x):
+    x.attach_grad()
+
     print("l2(x) = {}, l2^2(x) = {}".format(np.linalg.norm(x), np.linalg.norm(x) ** 2))
     with autograd.record():
-        y = 2 * np.dot(x, x)
+        y = f(x)
     y.backward()
     print("a={}, y(x)|_a = {}".format(x, y))
-    print("gradient of y(x) = 2 * np.dot(x, x) in x={} : {}".format(x, x.grad))
-    print("test gradient : {}".format(x.grad == 4 * x))
+    print("gradient of f in x={} : {}".format(x, x.grad))
+    print("test gradient : {}".format(x.grad == 2 * x))
 
 
 def main():
-    gradient()
+    x = np.arange(5)
+    grad_f_at(x)
 
 
 if __name__ == '__main__':
