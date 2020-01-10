963bbc5a377e7fcf75af050e23d73aac983bfef0
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2020-01-10 09:15:17 +0100

    gluon `VAE` class with `init_vae.py`

---
+++ autoencoders/gluon/init_vae.py
@@ -0,0 +1,72 @@
+import mxnet as mx
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+from mxnet import npx
+
+batch_size = 100
+mnist = mx.test_utils.get_mnist()
+train_data = np.reshape(mnist['train_data'], (-1, 28 * 28))
+test_data = np.reshape(mnist['test_data'], (-1, 28 * 28))
+
+
+# used in naive_bayes.py together with alternative way of loading MNIST dataset
+# keep it here for comparison
+# def transform(data, label):
+#     return nd.floor(data / 128).astype('float32').squeeze(axis=-1), label
+#
+# mnist_train = gluon.data.vision.MNIST(train=True, transform=transform)
+# mnist_test = gluon.data.vision.MNIST(train=False, transform=transform)
+# train_images, train_labels = mnist_train[:]
+
+
+def gpu_exists():
+    try:
+        mx.nd.zeros((1,), ctx=mx.gpu(0))
+    except:
+        return False
+    return True
+
+
+def get_model_ctx():
+    if gpu_exists():
+        print('Using GPU for model_ctx')
+        return mx.gpu(0)
+    else:
+        print('Using CPU for model_ctx')
+        return mx.cpu()
+
+
+def show_mnist():
+    mx.random.seed(1)
+    print('Test data labels shape : {}'.format(mnist['test_label'].shape))
+    print('Train data labels shape : {}'.format(mnist['train_label'].shape))
+    n_samples = 10
+    idx = np.random.choice(len(mnist['train_data']), n_samples)
+    _, axarr = plt.subplots(1, n_samples, figsize=(16, 4))
+    for i, j in enumerate(idx):
+        axarr[i].imshow(mnist['train_data'][j][0], cmap='Greys')
+        axarr[i].get_xaxis().set_ticks([])
+        axarr[i].get_yaxis().set_ticks([])
+    plt.show()
+
+
+def set_params():
+    n_batches = train_data.shape[0] / batch_size
+    train_iter = mx.io.NDArrayIter(data={'data': train_data}, label={'label': mnist['train_label']},
+                                   batch_size=batch_size)
+    test_iter = mx.io.NDArrayIter(data={'data': test_data}, label={'label': mnist['test_label']},
+                                  batch_size=batch_size)
+    print('parameters : n_batches = {}'.format(n_batches))
+
+
+
+def main():
+    show_mnist()
+    set_params()
+
+
+if __name__ == '__main__':
+    npx.set_np()
+    main()
+    sys.exit(0)
--- autoencoders/gluon/vae.py
+++ autoencoders/gluon/vae.py
@@ -1,62 +1,47 @@
-import mxnet as mx
-import numpy as np
-import sys
-from matplotlib import pyplot as plt
-from mxnet import npx, nd
+# from https://gluon.mxnet.io/chapter13_unsupervised-learning/vae-gluon.html
+from mxnet import gluon
+from mxnet.gluon import nn
+
+import autoencoders.gluon.init_vae as init_vae
 
 
-def gpu_exists():
-    try:
-        mx.nd.zeros((1,), ctx=mx.gpu(0))
-    except:
-        return False
-    return True
+class VAE(gluon.HybridBlock):
+    def __init__(self, n_hidden=400, n_latent=2, n_layers=1, n_output=784, batch_size=100, act_type='relu', **kwargs):
+        self.soft_zero = 1e-10
+        self.n_latent = n_latent
+        self.batch_size = batch_size
+        self.output = None
+        self.mu = None
+        # note to self: requring batch_size in model definition is sad, not sure how to deal with this otherwise though
+        super(VAE, self).__init__(**kwargs)
+        # self.use_aux_logits = use_aux_logits
+        with self.name_scope():
+            self.encoder = nn.HybridSequential(prefix='encoder')
+            for i in range(n_layers):
+                self.encoder.add(nn.Dense(n_hidden, activation=act_type))
+            self.encoder.add(nn.Dense(n_latent * 2, activation=None))
 
+            self.decoder = nn.HybridSequential(prefix='decoder')
+            for i in range(n_layers):
+                self.decoder.add(nn.Dense(n_hidden, activation=act_type))
+            self.decoder.add(nn.Dense(n_output, activation='sigmoid'))
 
-data_ctx = mx.cpu()
+    def hybrid_forward(self, F, x):
+        h = self.encoder(x)
+        # print(h)
+        mu_lv = F.split(h, axis=1, num_outputs=2)
+        mu = mu_lv[0]
+        lv = mu_lv[1]
+        self.mu = mu
+        # eps = F.random_normal(loc=0, scale=1, shape=mu.shape, ctx=model_ctx)
+        # this would work fine only for nd (i.e. non-hybridized block)
+        eps = F.random_normal(loc=0, scale=1, shape=(self.batch_size, self.n_latent), ctx=init_vae.get_model_ctx())
+        z = mu + F.exp(0.5 * lv) * eps
+        y = self.decoder(z)
+        self.output = y
 
+        KL = 0.5 * F.sum(1 + lv - mu * mu - F.exp(lv), axis=1)
+        logloss = F.sum(x * F.log(y + self.soft_zero) + (1 - x) * F.log(1 - y + self.soft_zero), axis=1)
+        loss = -logloss - KL
 
-def transform(data, label):
-    return nd.floor(data / 128).astype('float32').squeeze(axis=-1), label
-
-
-mnist = mx.test_utils.get_mnist()
-# mnist_train = gluon.data.vision.MNIST(train=True, transform=transform)
-# mnist_test = gluon.data.vision.MNIST(train=False, transform=transform)
-train_data = np.reshape(mnist['train_data'], (-1, 28 * 28))
-test_data = np.reshape(mnist['test_data'], (-1, 28 * 28))
-
-if gpu_exists():
-    print('Using GPU for model_ctx')
-    model_ctx = mx.gpu(0)
-else:
-    print('Using CPU for model_ctx')
-    model_ctx = mx.cpu()
-
-
-def show_mnist():
-    mx.random.seed(1)
-    output_fig = False
-    mnist = mx.test_utils.get_mnist()
-    # print(mnist['train_data'][0].shape)
-    # plt.imshow(mnist['train_data'][0][0],cmap='Greys')
-
-    n_samples = 10
-    idx = np.random.choice(len(mnist['train_data']), n_samples)
-    _, axarr = plt.subplots(1, n_samples, figsize=(16, 4))
-    for i, j in enumerate(idx):
-        axarr[i].imshow(mnist['train_data'][j][0], cmap='Greys')
-        # axarr[i].axis('off')
-        axarr[i].get_xaxis().set_ticks([])
-        axarr[i].get_yaxis().set_ticks([])
-    plt.show()
-
-
-def main():
-    show_mnist()
-
-
-if __name__ == '__main__':
-    npx.set_np()
-    main()
-    sys.exit(0)
+        return loss
