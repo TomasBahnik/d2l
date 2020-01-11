313d745171a4c2b745e5d2f295fc016543ba0fc6
Tomas Bahnik <tomas.bahnik@email.cz>
2020-01-11 20:58:02 +0100

    `synthetic_data_local` in common

--- common.py
+++ common.py
@@ -1,3 +1,6 @@
+from mxnet import np
+
+
 def model_errors(w, true_w, b, true_b):
     rw = w.reshape(true_w.shape)
     aew = true_w - rw
@@ -11,3 +14,11 @@ def model_errors(w, true_w, b, true_b):
     print('Absolute error of bias : {}'.format(aeb))
     print('Relative error of weights : {} %'.format(rew))
     print('Relative error of bias : {} %'.format(reb))
+
+
+def synthetic_data_local(w, b, num_examples):
+    """generate y = X w + b + noise"""
+    X = np.random.normal(0, 1, (num_examples, len(w)))
+    y = np.dot(X, w) + b
+    y += np.random.normal(0, 0.01, y.shape)
+    return X, y
