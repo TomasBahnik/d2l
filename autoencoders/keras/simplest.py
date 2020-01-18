bcd118a59b8f5b455bf5910f29ad80ef09e1d2a2
Tomas Bahnik <tomas.bahnik@email.cz>
2020-01-18 19:14:32 +0100

    merged simplest keras autoencoder

--- autoencoders/keras/simplest.py
+++ autoencoders/keras/simplest.py
@@ -1,3 +1,5 @@
+import numpy as np
+from keras.datasets import mnist
 from keras.layers import Input, Dense
 from keras.models import Model
 
@@ -17,4 +19,29 @@ autoencoder = Model(input_img, decoded)
 # encoder model
 # this model maps an input to its encoded representation
 encoder = Model(input_img, encoded)
+
+# decoder model
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+(x_train, _), (x_test, _) = mnist.load_data()
+
+# transform
+# normalize all values between 0 and 1 and we will flatten the 28x28 images into vectors of size 784.
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print(x_train.shape)
+print(x_test.shape)
+
+# train our autoencoder for 50 epochs:
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
