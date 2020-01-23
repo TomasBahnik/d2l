c6523ee623802c3d7d190a75113c27beab65321d
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2020-01-23 18:15:05 +0100

    keras gpu check

--- autoencoders/keras/check_gpu.py
+++ autoencoders/keras/check_gpu.py
@@ -8,3 +8,5 @@ print(" gpu available : {} ".format(tf.test.is_gpu_available()))  # True/False
 
 # Or only check for gpu's with cuda support
 print(" gpu cuda available : {} ".format(tf.test.is_gpu_available(cuda_only=True)))  # True/False
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
