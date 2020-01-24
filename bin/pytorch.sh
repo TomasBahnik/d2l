7fca247e25bb37b1b6784aaf031ab5883fb22f2b
Bahnik, Tomas <tomas.bahnik@ataccama.com>
2020-01-24 09:13:28 +0100

    add pytorch.sh install script

---
+++ bin/pytorch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script for creating gluon conda env
+#  * env name  is teh 1st argument (via env.sh). If it contains gpu
+#    GPU version is installed
+SCRIPT=$(realpath "$0")
+WORK_DIR=$(dirname "$SCRIPT")
+
+# https://github.com/koalaman/shellcheck/wiki/SC1090
+# shellcheck source=env.sh
+source "$WORK_DIR"/env.sh
+
+conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+if [[ "$CONDA_ENV" == *"gpu"* ]]; then
+  logger "Install pytorch GPU"
+  conda install -c anaconda pytorch-gpu
+fi
+logger "List installed pillow"
+conda list PIL
+# pytorch impl of VAE : PIL `7.x` causes error `cannot import name 'PILLOW_VERSION' from 'PIL'
+logger "Install pillow 6.1 used by VAE"
+conda install pillow==6.1
